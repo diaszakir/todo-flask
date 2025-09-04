1f1050960a4714078fbb853d88b045f127084f4a
Dias Zakir <diaszakir1@outlook.com>
2025-09-04 17:26:02 +0500

    error handling and some fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from resources.task import blp as TaskBlueprint
 from resources.user import blp as UserBlueprint
@@ -27,6 +27,33 @@ def create_app(db_url=None):
   db.init_app(app)
   migrate = Migrate(app, db)
   api = Api(app)
+
+  @jwt.expired_token_loader
+  def expired_token_callback(jwt_header, jwt_payload):
+      return jsonify(
+          {
+              "message": "The token has expired",
+              "error": "token_expired"
+          }
+      ), 401
+
+  @jwt.invalid_token_loader
+  def invalid_token_callback(error):
+      return jsonify(
+          {
+              "message": "Signature verification failed",
+              "error": "invalid_token"
+          }
+      ), 401
+
+  @jwt.unauthorized_loader
+  def missing_token_callback(error):
+      return jsonify(
+          {
+              "description": "Request does not contain an access token",
+              "error": "authorization_required"
+          }
+      ), 401
 
   # Don't needed after Migrate
   # with app.app_context():
